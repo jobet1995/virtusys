ec16a40e227179250efa3a51ef8e33310bf3d4ee
jobet1995 <jobetcasquejo221@gmail.com>
2025-11-05 17:08:21 +0800

    Initialize

---
+++ virtusys/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4cn*=)y_of!!=!cw2!8d=77q#n%4p63(kd$o630&llw18)0g+h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
